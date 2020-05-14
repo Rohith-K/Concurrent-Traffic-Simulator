f4837d261a6c4e60cd37191c1e4a6820df3faa9f
Rohith <krohith@clemson.edu>
2020-05-14 22:57:39 +0000

    TrafficLight class partial implementation

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -30,7 +30,12 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+enum TrafficLightPhase{
+    red,
+    green
+};
+
+class TrafficLight: public TrafficObject
 {
 public:
     // constructor / desctructor
@@ -38,9 +43,13 @@ public:
     // getters / setters
 
     // typical behaviour methods
+    void waitForGreen();
+    void simulate();
+    TrafficLightPhase getCurrentPhase();
 
 private:
     // typical behaviour methods
+    void cycleThroughPhases();
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
@@ -48,6 +57,7 @@ private:
 
     std::condition_variable _condition;
     std::mutex _mutex;
+    TrafficLightPhase _currentPhase;
 };
 
 #endif
