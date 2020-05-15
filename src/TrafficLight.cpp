f715b81b8424933b538fdc9ab7866748f287536f
Rohith Krishnan <krohith@clemson.edu>
2020-05-15 00:27:42 -0400

    completed first implementation draft

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -12,7 +12,7 @@ T MessageQueue<T>::receive()
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
     std::unique_lock<std::mutex> ulock(MsgQ_mtx);
-    cv.wait(ulock, [this](){return !_queue.empty()});
+    cv.wait(ulock, [this](){return !_queue.empty();});
 
     T msg = std::move(_queue.front());
     _queue.pop_front();
